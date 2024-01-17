1bcdbeca71e30a369d01d5fbac7e7b5151b3c326
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-17 09:15:22 -0300

    Initial setup

---
+++ src/test/java/com/ufrn/nei/almoxarifadoapi/AlmoxarifadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.nei.almoxarifadoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxarifadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
