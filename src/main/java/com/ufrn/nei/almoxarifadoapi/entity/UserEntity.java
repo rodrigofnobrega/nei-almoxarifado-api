b8d0621e572dda0b05a449e13a2f94236d94dc39
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-26 16:51:43 -0300

    feat: Adicionar entidade User

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/entity/UserEntity.java
@@ -0,0 +1,58 @@
+package com.ufrn.nei.almoxarifadoapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Usuarios")
+public class UserEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome", nullable = false, unique = false, length = 255)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, unique = false, length = 255)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role", nullable = false, unique = false)
+    private RoleEntity role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(email, that.email) && Objects.equals(password, that.password) && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", idRole=" + role +
+                '}';
+    }
+}
