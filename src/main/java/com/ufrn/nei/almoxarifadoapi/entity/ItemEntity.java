839399018a7d39e214e6b8fc7d7a30d29a30c6d2
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-26 17:02:43 -0300

    feat: Gerando os métodos equals, hashcode, toString

--- src/main/java/com/ufrn/nei/almoxarifadoapi/entity/ItemEntity.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/entity/ItemEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -26,4 +27,27 @@ public class ItemEntity implements Serializable {
 
     @Column(name = "disponivel", nullable = false, unique = false)
     private Boolean available = Boolean.TRUE;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemEntity that = (ItemEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(itemTagging, that.itemTagging) && Objects.equals(available, that.available);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, itemTagging, available);
+    }
+
+    @Override
+    public String toString() {
+        return "ItemEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", itemTagging=" + itemTagging +
+                ", available=" + available +
+                '}';
+    }
 }
--- src/main/java/com/ufrn/nei/almoxarifadoapi/entity/RoleEntity.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/entity/RoleEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,4 +21,25 @@ public class RoleEntity implements Serializable {
 
     @Column(name = "encargo", nullable = false, unique = false, length = 50)
     private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+
+    @Override
+    public String toString() {
+        return "RoleEntity{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
