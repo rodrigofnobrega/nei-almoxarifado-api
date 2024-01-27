aedc787113a4e318f1a2a5c221ce6e86bb7a0dce
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-27 14:44:06 -0300

    feat: Criando DTO para a resposta de requisições das Roles

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/RoleResponseDto.java
@@ -0,0 +1,13 @@
+package com.ufrn.nei.almoxarifadoapi.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleResponseDto {
+    private String id;
+    private String role;
+}
