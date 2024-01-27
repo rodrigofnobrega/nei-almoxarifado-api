7a2deafe1049fcec1deaab3407b88d2b979c0327
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-27 14:44:50 -0300

    feat: Criando DTO para atualizar o nome de uma Role

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/RoleUpdateDto.java
@@ -0,0 +1,12 @@
+package com.ufrn.nei.almoxarifadoapi.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleUpdateDto {
+    private String newRole;
+}
