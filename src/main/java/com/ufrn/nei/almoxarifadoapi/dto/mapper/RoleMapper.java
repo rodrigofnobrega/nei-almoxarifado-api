12681579d52fe572d50a4d60d3d09299348c91f3
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-27 14:46:59 -0300

    feat: Adicionando o recurso de conversão de DTOs para Entitys

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/RoleMapper.java
@@ -0,0 +1,27 @@
+package com.ufrn.nei.almoxarifadoapi.dto.mapper;
+
+import com.ufrn.nei.almoxarifadoapi.dto.RoleCreateDto;
+import com.ufrn.nei.almoxarifadoapi.dto.RoleResponseDto;
+import com.ufrn.nei.almoxarifadoapi.entity.RoleEntity;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RoleMapper {
+    public static RoleEntity toRole(RoleCreateDto roleCreateDto) {
+        return new ModelMapper().map(roleCreateDto, RoleEntity.class);
+    }
+
+    public static RoleEntity toRole(RoleResponseDto roleCreateDto) {
+        return new ModelMapper().map(roleCreateDto, RoleEntity.class);
+    }
+
+    public static RoleResponseDto toResponseDto(RoleEntity roleEntity) {
+        return new ModelMapper().map(roleEntity, RoleResponseDto.class);
+    }
+
+    public static List<RoleResponseDto> toListResponseDto(List <RoleEntity> roles) {
+        return roles.stream().map(role -> toResponseDto(role)).collect(Collectors.toList());
+    }
+}
