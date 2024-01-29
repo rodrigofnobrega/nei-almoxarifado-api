f8e84512f440a5067e828db602297622f0c4ff7c
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-29 11:25:29 -0300

    test: Criação de teste para atualizar nome da Role

--- src/test/java/com/ufrn/nei/almoxarifadoapi/service/RoleServiceTest.java
+++ src/test/java/com/ufrn/nei/almoxarifadoapi/service/RoleServiceTest.java
@@ -1,6 +1,8 @@
 package com.ufrn.nei.almoxarifadoapi.service;
 
 import com.ufrn.nei.almoxarifadoapi.dto.RoleResponseDto;
+import com.ufrn.nei.almoxarifadoapi.dto.RoleUpdateDto;
+import com.ufrn.nei.almoxarifadoapi.dto.mapper.RoleMapper;
 import com.ufrn.nei.almoxarifadoapi.entity.RoleEntity;
 import com.ufrn.nei.almoxarifadoapi.repository.RoleRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,11 +28,11 @@ class RoleServiceTest {
     @InjectMocks
     private RoleService roleService;
 
-    private RoleEntity roleEntity;
+    private RoleEntity role;
 
     @BeforeEach
     public void setup() {
-        roleEntity = createRole();
+        role = createRole();
         MockitoAnnotations.openMocks(this);
     }
 
@@ -42,28 +44,35 @@ class RoleServiceTest {
         return roleEntity;
     }
 
+    private RoleUpdateDto createRoleResponseDto() {
+        RoleUpdateDto roleDto = new RoleUpdateDto();
+        roleDto.setNewRole("Admin");
+
+        return roleDto;
+    }
+
     @Test
     @DisplayName("Testa a criação de novas roles")
     void saveRoleTest() {
-        when(roleRepository.save(roleEntity)).thenReturn(roleEntity);
+        when(roleRepository.save(role)).thenReturn(role);
 
-        RoleResponseDto savedRole = roleService.save(roleEntity);
+        RoleResponseDto savedRole = roleService.save(role);
 
-        verify(roleRepository, times(1)).save(roleEntity);
+        verify(roleRepository, times(1)).save(role);
 
         assertNotNull(savedRole);
 
-        assertEquals(roleEntity.getId(), savedRole.getId());
-        assertEquals(roleEntity.getRole(), savedRole.getRole());
+        assertEquals(role.getId(), savedRole.getId());
+        assertEquals(role.getRole(), savedRole.getRole());
     }
 
     @Test
     @DisplayName("Testa encontrar uma Role pelo ID no serviço")
     @Transactional(readOnly = true)
     void testFindRoleById() {
-        Long roleId = roleEntity.getId();
+        Long roleId = role.getId();
 
-        when(roleRepository.findById(roleId)).thenReturn(Optional.of(roleEntity));
+        when(roleRepository.findById(roleId)).thenReturn(Optional.of(role));
 
         RoleResponseDto foundRole = roleService.findById(roleId);
 
@@ -71,15 +80,15 @@ class RoleServiceTest {
 
         assertNotNull(foundRole);
 
-        assertEquals(roleEntity.getId(), foundRole.getId());
-        assertEquals(roleEntity.getRole(), foundRole.getRole());
+        assertEquals(role.getId(), foundRole.getId());
+        assertEquals(role.getRole(), foundRole.getRole());
     }
 
     @Test
     @DisplayName("Teste para encontrar todas as roles cadastradas")
     @Transactional(readOnly = true)
     void testFindAllRoles() {
-        when(roleRepository.findAll()).thenReturn(Collections.singletonList(roleEntity));
+        when(roleRepository.findAll()).thenReturn(Collections.singletonList(role));
 
         List<RoleResponseDto> roleResponse = roleService.findAllRoles();
 
@@ -87,7 +96,25 @@ class RoleServiceTest {
 
         assertNotNull(roleResponse);
 
-        assertEquals(roleEntity.getRole(), roleResponse.get(0).getRole());
-        assertEquals(roleEntity.getId(), roleResponse.get(0).getId());
+        assertEquals(role.getRole(), roleResponse.get(0).getRole());
+        assertEquals(role.getId(), roleResponse.get(0).getId());
+    }
+
+    @Test
+    @DisplayName("Teste para mudar o nome de uma role pelo ID")
+    void testRenameRole() {
+        RoleUpdateDto newRole = createRoleResponseDto();
+
+        when(roleRepository.findById(role.getId())).thenReturn(Optional.of(role));
+
+        when(roleRepository.save(any(RoleEntity.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        RoleResponseDto response = roleService.updateRoleById(role.getId(), newRole);
+
+        verify(roleRepository, times(1)).findById(role.getId());
+        verify(roleRepository, times(1)).save(any(RoleEntity.class));
+
+        assertEquals(role.getId(), response.getId());
+        assertEquals(newRole.getNewRole(), response.getRole());
     }
 }
