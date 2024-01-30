ba311c861124f988d4a8fe795239d0e43f31b9b0
leirdan <uouleirdan@gmail.com>
2024-01-30 15:41:44 -0300

    feat: funcionalidades básicas de itens

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/controller/ItemController.java
@@ -0,0 +1,57 @@
+package com.ufrn.nei.almoxarifadoapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufrn.nei.almoxarifadoapi.dto.item.ItemCreateDTO;
+import com.ufrn.nei.almoxarifadoapi.dto.item.ItemDetailsDTO;
+import com.ufrn.nei.almoxarifadoapi.dto.item.ItemUpdateDTO;
+import com.ufrn.nei.almoxarifadoapi.service.ItemService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/itens")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<List<ItemDetailsDTO>> getAllItems() {
+        List<ItemDetailsDTO> items = itemService.findAllItems();
+
+        return ResponseEntity.status(HttpStatus.OK).body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemDetailsDTO> getItem(@PathVariable Long id) {
+        ItemDetailsDTO item = itemService.findItem(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(item);
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDetailsDTO> createItem(@RequestBody @Valid ItemCreateDTO itemDTO) {
+        ItemDetailsDTO item = itemService.createItem(itemDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(item);
+    }
+
+    @PutMapping
+    public ResponseEntity<ItemDetailsDTO> updateItem(@RequestBody @Valid ItemUpdateDTO itemDTO) {
+        ItemDetailsDTO item = itemService.updateItem(itemDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(item);
+    }
+
+}
