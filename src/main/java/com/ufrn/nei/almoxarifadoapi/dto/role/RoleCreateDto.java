6b9c5aa66d212cd960b068cf03dc13db3fa974c1
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-30 18:48:23 -0300

    refactor: Criação dos métodos utilitários e construtor com todos os argumentos para Role

--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/role/RoleCreateDto.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/role/RoleCreateDto.java
@@ -1,12 +1,36 @@
 package com.ufrn.nei.almoxarifadoapi.dto.role;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RoleCreateDto {
     private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleCreateDto that = (RoleCreateDto) o;
+        return Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(role);
+    }
+
+    @Override
+    public String toString() {
+        return "RoleCreateDto{" +
+                "role='" + role + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/role/RoleResponseDto.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/role/RoleResponseDto.java
@@ -1,13 +1,38 @@
 package com.ufrn.nei.almoxarifadoapi.dto.role;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RoleResponseDto {
     private Long id;
     private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleResponseDto that = (RoleResponseDto) o;
+        return Objects.equals(id, that.id) && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+
+    @Override
+    public String toString() {
+        return "RoleResponseDto{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
