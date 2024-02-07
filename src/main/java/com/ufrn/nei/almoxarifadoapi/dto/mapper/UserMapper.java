49826161d5d902a9dc38eb6bac1b7255c2219f2c
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-02-07 15:51:09 -0300

    feat: Criar DTOs e mapper para a entidade User

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.ufrn.nei.almoxarifadoapi.dto.mapper;
+
+import com.ufrn.nei.almoxarifadoapi.dto.user.UserResponseDTO;
+import com.ufrn.nei.almoxarifadoapi.entity.UserEntity;
+import org.modelmapper.ModelMapper;
+
+public class UserMapper {
+    public static UserResponseDTO toResponseDTO(UserEntity userEntity) {
+        return new ModelMapper().map(userEntity, UserResponseDTO.class);
+    }
+
+    public static UserEntity toItem(UserResponseDTO userDTO) {
+        return new ModelMapper().map(userDTO, UserEntity.class);
+    }
+}
