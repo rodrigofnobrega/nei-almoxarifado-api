e356a505ee558ec7efef1aeeb97fb6743bed0949
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-02-17 20:33:48 -0300

    feat: Adicionar no DTO de resposta os campos quantidade, operação e data

--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/record/RecordResponseDTO.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/record/RecordResponseDTO.java
@@ -2,10 +2,13 @@ package com.ufrn.nei.almoxarifadoapi.dto.record;
 
 import com.ufrn.nei.almoxarifadoapi.dto.item.ItemRecordDTO;
 import com.ufrn.nei.almoxarifadoapi.dto.user.UserRecordDTO;
+import com.ufrn.nei.almoxarifadoapi.enums.RecordOperationEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -15,4 +18,7 @@ public class RecordResponseDTO {
     private Long id;
     private UserRecordDTO user;
     private ItemRecordDTO item;
+    private Integer quantity;
+    private RecordOperationEnum operation;
+    private Timestamp data;
 }
