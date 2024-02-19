1ea626a60c48338326d71a9c2716996466c265e4
leirdan <uouleirdan@gmail.com>
2024-02-19 17:54:14 -0300

    fix: mudança de decorator em ItemUpdateDTO

--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/item/ItemUpdateDTO.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/item/ItemUpdateDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,7 @@ public class ItemUpdateDTO {
     String name;
     @Positive
     Long itemTagging;
-    @Positive
+    @PositiveOrZero
     int quantityLend;
 
     @Override
