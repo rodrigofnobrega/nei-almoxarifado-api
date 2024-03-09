9bdcc8ad76912c17027daeab8dbd2f37bbd05c8b
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-03-09 10:49:51 -0300

    feat: Criar métodos para criar e validar tokens

---
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/infra/jwt/JwtUtils.java
@@ -0,0 +1,94 @@
+package com.ufrn.nei.almoxarifadoapi.infra.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+@NoArgsConstructor
+public class JwtUtils {
+    public static final String JWT_BEARER = "Bearer ";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
+    public static final Long EXPIRE_DAYS = 0L;
+    public static final Long EXPIRE_HOURS = 0L;
+    public static final Long EXPIRE_MINUTES = 2L;
+
+    private static SecretKey generateKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate(Date start) {
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
+
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static JwtToken createToken(String email, String role) {
+        Date issuedAt = new Date();
+        Date limit = toExpireDate(issuedAt);
+
+        String token = Jwts.builder()
+                .header().add("typ", "JWT")
+                .and()
+                .subject(email)
+                .issuedAt(issuedAt)
+                .expiration(limit)
+                .signWith(generateKey())
+                .claim("role", role)
+                .compact();
+
+        return new JwtToken(token);
+    }
+
+    private static Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(generateKey())
+                    .build()
+                    .parseSignedClaims(refactorToken(token)).getPayload();
+        } catch (JwtException ex) {
+            log.error(String.format("Token inválido %s", ex.getMessage()));
+        }
+
+        return null;
+    }
+
+    public static String getUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public static Boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(generateKey())
+                    .build()
+                    .parseSignedClaims(refactorToken(token));
+            return Boolean.TRUE;
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalido %s", ex.getMessage()));
+        }
+
+        return Boolean.FALSE;
+    }
+
+    private static String refactorToken(String token) {
+        if (token.contains(JWT_BEARER)) {
+            return token.substring(JWT_BEARER.length());
+        }
+
+        return token;
+    }
+
+
+}
