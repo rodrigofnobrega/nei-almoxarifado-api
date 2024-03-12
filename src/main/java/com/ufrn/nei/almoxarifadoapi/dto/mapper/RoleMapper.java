a70cc8e5a8511b729819439dc0f767a7220fb1b3
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-03-12 08:48:19 -0300

    refactor: Configurar mapper de roles

--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/RoleMapper.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/RoleMapper.java
@@ -2,9 +2,14 @@ package com.ufrn.nei.almoxarifadoapi.dto.mapper;
 
 import com.ufrn.nei.almoxarifadoapi.dto.role.RoleCreateDto;
 import com.ufrn.nei.almoxarifadoapi.dto.role.RoleResponseDto;
+import com.ufrn.nei.almoxarifadoapi.dto.user.UserResponseDTO;
 import com.ufrn.nei.almoxarifadoapi.entity.RoleEntity;
+import com.ufrn.nei.almoxarifadoapi.entity.UserEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.security.core.parameters.P;
 
+import javax.management.relation.Role;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +23,25 @@ public class RoleMapper {
     }
 
     public static RoleResponseDto toResponseDto(RoleEntity roleEntity) {
-        return new ModelMapper().map(roleEntity, RoleResponseDto.class);
+        String role = roleEntity.getRole();
+
+        if (role.contains("ROLE_")) {
+            role = roleEntity.getRole().substring("ROLE_".length());
+        }
+
+        String finalRole = role;
+
+        PropertyMap<RoleEntity, RoleResponseDto> propertyMap = new PropertyMap<>() {
+            @Override
+            protected void configure() {
+                map().setRole(finalRole);
+            }
+        };
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addMappings(propertyMap);
+
+        return modelMapper.map(roleEntity, RoleResponseDto.class);
     }
 
     public static List<RoleResponseDto> toListResponseDto(List <RoleEntity> roles) {
