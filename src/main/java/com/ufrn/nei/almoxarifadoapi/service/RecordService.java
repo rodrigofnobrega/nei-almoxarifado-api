4eb22fd5fc481862fa03e4a6ca67efaa3075e824
leirdan <uouleirdan@gmail.com>
2024-03-18 21:51:40 -0300

    feat: alterações referentes a registros

--- src/main/java/com/ufrn/nei/almoxarifadoapi/service/RecordService.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/service/RecordService.java
@@ -1,6 +1,5 @@
 package com.ufrn.nei.almoxarifadoapi.service;
 
-import com.ufrn.nei.almoxarifadoapi.dto.mapper.ItemMapper;
 import com.ufrn.nei.almoxarifadoapi.dto.record.RecordCreateDTO;
 import com.ufrn.nei.almoxarifadoapi.dto.record.RecordCreateItemDTO;
 import com.ufrn.nei.almoxarifadoapi.entity.ItemEntity;
@@ -9,14 +8,12 @@ import com.ufrn.nei.almoxarifadoapi.entity.UserEntity;
 import com.ufrn.nei.almoxarifadoapi.enums.RecordOperationEnum;
 import com.ufrn.nei.almoxarifadoapi.exception.EntityNotFoundException;
 import com.ufrn.nei.almoxarifadoapi.repository.RecordRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Slf4j
 @Service
 public class RecordService {
     @Autowired
@@ -27,22 +24,26 @@ public class RecordService {
 
     @Autowired
     private ItemService itemService;
-    
+
     @Transactional
     public RecordEntity save(RecordCreateDTO recordCreateDTO, RecordOperationEnum operationEnum) {
         UserEntity user = userService.findById(recordCreateDTO.getUserID());
-        ItemEntity item = ItemMapper.toItem(itemService.findById(recordCreateDTO.getItemID()));
+        ItemEntity item = itemService.findById(recordCreateDTO.getItemID());
         RecordEntity record = new RecordEntity(user, item, recordCreateDTO.getQuantity(), operationEnum);
 
-        return recordRepository.save(record);
+        record = recordRepository.save(record);
+
+        return record;
     }
 
     @Transactional
     public RecordEntity save(RecordCreateItemDTO createItemDTO, ItemEntity item, RecordOperationEnum operationEnum) {
         UserEntity user = userService.findById(createItemDTO.getUserID());
-        RecordEntity record = new RecordEntity(user, item, createItemDTO.getItem().getQuantityAvailable(), operationEnum);
+        RecordEntity record = new RecordEntity(user, item, createItemDTO.getItem().getQuantity(), operationEnum);
 
-        return recordRepository.save(record);
+        record = recordRepository.save(record);
+
+        return record;
     }
 
     @Transactional(readOnly = true)
@@ -53,8 +54,7 @@ public class RecordService {
     @Transactional(readOnly = true)
     public RecordEntity findById(Long id) {
         return recordRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(String.format("Usuário não encontrado com o id='%s'", id))
-        );
+                () -> new EntityNotFoundException(String.format("Registro não encontrado com o id='%s'", id)));
     }
 
     @Transactional(readOnly = true)
