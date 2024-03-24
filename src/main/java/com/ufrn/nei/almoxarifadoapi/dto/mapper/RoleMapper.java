d287ebaebedc96326594f2e93e2eba216ccebc64
leirdan <uouleirdan@gmail.com>
2024-03-24 20:44:16 -0300

    feat: nova conversão em RoleMapper

--- src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/RoleMapper.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/dto/mapper/RoleMapper.java
@@ -13,15 +13,19 @@ public class RoleMapper {
         return new ModelMapper().map(roleCreateDto, RoleEntity.class);
     }
 
-    public static RoleEntity toRole(RoleResponseDto roleCreateDto) {
-        return new ModelMapper().map(roleCreateDto, RoleEntity.class);
+    public static RoleEntity toRole(RoleResponseDto roleResponseDto) {
+        return new ModelMapper().map(roleResponseDto, RoleEntity.class);
     }
 
     public static RoleResponseDto toResponseDto(RoleEntity roleEntity) {
         return new ModelMapper().map(roleEntity, RoleResponseDto.class);
     }
 
-    public static List<RoleResponseDto> toListResponseDto(List <RoleEntity> roles) {
+    public static List<RoleResponseDto> toListResponseDto(List<RoleEntity> roles) {
         return roles.stream().map(role -> toResponseDto(role)).collect(Collectors.toList());
+    }
+
+    public static List<RoleEntity> toListRole(List<RoleResponseDto> rolesDto) {
+        return rolesDto.stream().map(dto -> toRole(dto)).collect(Collectors.toList());
     }
 }
