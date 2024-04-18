73195e043521b90c5a681378b88828ca6f8c8486
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-04-18 13:45:22 -0300

    refactor: Utilizar componente para remover o prefixo da ROLE

--- src/main/java/com/ufrn/nei/almoxarifadoapi/repository/projection/UserProjection.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/repository/projection/UserProjection.java
@@ -2,6 +2,8 @@ package com.ufrn.nei.almoxarifadoapi.repository.projection;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufrn.nei.almoxarifadoapi.entity.RoleEntity;
+import com.ufrn.nei.almoxarifadoapi.repository.projection.component.RemoveRolePrefixComponent;
 
 public interface UserProjection {
     Long getId();
@@ -10,12 +12,9 @@ public interface UserProjection {
     Boolean getActive();
     @JsonIgnore
     RoleEntity getRole();
-    interface RoleEntity {
-        String getRole();
-    }
 
     @JsonProperty("role")
-    default String getRoleWithoutPrefix() {
-        return getRole().getRole().substring("ROLE_".length());
+    default String removeRolePrefix() {
+        return RemoveRolePrefixComponent.getRoleWithoutPrefix(getRole());
     }
 }
