fd63f6f1ae548171b9924dd314be8cc0a71b6d6e
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-04-30 18:47:05 -0300

    refactor: Utilizar text block para as mensagens do email

--- src/main/java/com/ufrn/nei/almoxarifadoapi/infra/mail/MailTemplates.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/infra/mail/MailTemplates.java
@@ -14,19 +14,27 @@ public class MailTemplates {
 
     public SimpleMailMessage buildMailMessageUserCreated(String userEmail, String userName) {
         String subject = "Sua conta foi criada com sucesso!";
-        String text = String.format("Olá %s,\n\n"
-                + "Seja bem-vindo ao nosso sistema de solicitação de itens do almoxarifado! Estamos muito felizes em tê-lo conosco.\n\n"
-                + "Com este sistema, você terá acesso a uma variedade de itens disponíveis em nosso almoxarifado, tornando mais fácil e conveniente solicitar o que você precisa para suas atividades.\n\n"
-                + "Aqui estão algumas coisas que você pode fazer com a sua nova conta:\n"
-                + "- Explorar nosso catálogo de itens disponíveis.\n"
-                + "- Criar solicitações para itens que você precisa.\n"
-                + "- Acompanhar o status das suas solicitações.\n"
-                + "- Receber notificações sobre o progresso das suas solicitações.\n\n"
-                + "Estamos sempre trabalhando para melhorar nossa plataforma e proporcionar uma experiência mais eficiente para todos os nossos usuários.\n\n"
-                + "Se precisar de ajuda ou tiver alguma dúvida, não hesite em entrar em contato conosco. Estamos aqui para ajudar!\n\n"
-                + "Mais uma vez, obrigado por se juntar a nós. Esperamos que você aproveite ao máximo o nosso sistema de solicitação de itens do almoxarifado.\n\n"
-                + "Atenciosamente,\n"
-                + "Equipe do NEI", userName);
+        String text = String.format("""
+                Olá %s,
+
+                Seja bem-vindo ao nosso sistema de solicitação de itens do almoxarifado! Estamos muito felizes em tê-lo conosco.
+
+                Com este sistema, você terá acesso a uma variedade de itens disponíveis em nosso almoxarifado, tornando mais fácil e conveniente solicitar o que você precisa para suas atividades.
+
+                Aqui estão algumas coisas que você pode fazer com a sua nova conta:
+                - Explorar nosso catálogo de itens disponíveis.
+                - Criar solicitações para itens que você precisa.
+                - Acompanhar o status das suas solicitações.
+                - Receber notificações sobre o progresso das suas solicitações.
+
+                Estamos sempre trabalhando para melhorar nossa plataforma e proporcionar uma experiência mais eficiente para todos os nossos usuários.
+
+                Se precisar de ajuda ou tiver alguma dúvida, não hesite em entrar em contato conosco. Estamos aqui para ajudar!
+
+                Mais uma vez, obrigado por se juntar a nós. Esperamos que você aproveite ao máximo o nosso sistema de solicitação de itens do almoxarifado.
+
+                Atenciosamente,
+                Equipe do NEI""", userName);
 
         simpleMailMessage.setTo(userEmail);
         simpleMailMessage.setSubject(subject);
@@ -49,13 +57,17 @@ public class MailTemplates {
         String formatDate = RefactorDate.refactorTimestamp(date);
 
         String subject = "Sua Solicitação foi criada com sucesso!";
-        String text = String.format("Olá %s,\n\n"
-                        + "Sua solicitação do item '%s' foi realizada com sucesso!\n\n"
-                        + "Detalhes:\n"
-                        + "- Item: %s\n"
-                        + "- Quantidade: %d\n"
-                        + "- Hora da Solicitação: %s\n\n"
-                        + "Obrigado por utilizar nosso sistema.",
+        String text = String.format("""
+                        Olá %s,
+
+                        Sua solicitação do item '%s' foi realizada com sucesso!
+
+                        Detalhes:
+                        - Item: %s
+                        - Quantidade: %d
+                        - Hora da Solicitação: %s
+
+                        Obrigado por utilizar nosso sistema.""",
                 userName, itemName, itemName, itemQuantity, formatDate);
 
         simpleMailMessage.setTo(userEmail);
@@ -70,13 +82,17 @@ public class MailTemplates {
         String formatDate = RefactorDate.refactorTimestamp(date);
 
         String subject = "Sua solicitação foi aceita!";
-        String text = String.format("Olá %s,\n\n"
-                        + "Sua solicitação do item '%s' foi aceita!\n\n"
-                        + "Detalhes:\n"
-                        + "- Item: %s\n"
-                        + "- Quantidade: %d\n"
-                        + "- Hora da Aceitação: %s\n\n"
-                        + "Obrigado por utilizar nosso sistema.",
+        String text = String.format("""
+                        Olá %s,
+
+                        Sua solicitação do item '%s' foi aceita!
+
+                        Detalhes:
+                        - Item: %s
+                        - Quantidade: %d
+                        - Hora da Aceitação: %s
+
+                        Obrigado por utilizar nosso sistema.""",
                 userName, itemName, itemName, itemQuantity, formatDate);
 
         simpleMailMessage.setTo(userEmail);
@@ -91,15 +107,20 @@ public class MailTemplates {
         String formatDate = RefactorDate.refactorTimestamp(date);
 
         String subject = "Sua solicitação foi recusada.";
-        String text = String.format("Olá %s,\n\n"
-                        + "Sua solicitação para o item '%s' foi recusada.\n\n"
-                        + "Detalhes:\n"
-                        + "- Item: %s\n"
-                        + "- Quantidade: %d\n"
-                        + "- Hora da Recusa: %s\n\n"
-                        + "Por favor, entre em contato conosco para mais informações.\n\n"
-                        + "Atenciosamente,\n"
-                        + "Equipe do Almoxarifado",
+        String text = String.format("""
+                        Olá %s,
+
+                        Sua solicitação para o item '%s' foi recusada.
+
+                        Detalhes:
+                        - Item: %s
+                        - Quantidade: %d
+                        - Hora da Recusa: %s
+
+                        Por favor, entre em contato conosco para mais informações.
+
+                        Atenciosamente,
+                        Equipe do Almoxarifado""",
                 userName, itemName, itemName, itemQuantity, formatDate);
 
         simpleMailMessage.setTo(userEmail);
@@ -114,13 +135,17 @@ public class MailTemplates {
         String formatDate = RefactorDate.refactorTimestamp(date);
 
         String subject = "Confirmação de cancelamento.";
-        String text = String.format("Olá %s,\n\n"
-                        + "Sua solicitação para o item '%s' foi cancelada com sucesso.\n\n"
-                        + "Detalhes:\n"
-                        + "- Item: %s\n"
-                        + "- Quantidade: %d\n"
-                        + "- Hora do Cancelamento: %s\n\n"
-                        + "Equipe do Almoxarifado.",
+        String text = String.format("""
+                        Olá %s,
+
+                        Sua solicitação para o item '%s' foi cancelada com sucesso.
+
+                        Detalhes:
+                        - Item: %s
+                        - Quantidade: %d
+                        - Hora do Cancelamento: %s
+
+                        Equipe do Almoxarifado.""",
                 userName, itemName, itemName, itemQuantity, formatDate);
 
         simpleMailMessage.setTo(userEmail);
