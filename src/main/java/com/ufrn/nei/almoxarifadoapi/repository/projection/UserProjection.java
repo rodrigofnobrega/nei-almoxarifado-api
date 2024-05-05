89744d42fd04cb4646d1a015b2dd7904d5398f23
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-05-05 17:37:29 -0300

    feat: Adicionar campo 'existRecord' ao listar todos os usuários

--- src/main/java/com/ufrn/nei/almoxarifadoapi/repository/projection/UserProjection.java
+++ src/main/java/com/ufrn/nei/almoxarifadoapi/repository/projection/UserProjection.java
@@ -2,8 +2,11 @@ package com.ufrn.nei.almoxarifadoapi.repository.projection;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufrn.nei.almoxarifadoapi.entity.RecordEntity;
 import com.ufrn.nei.almoxarifadoapi.entity.RoleEntity;
 import com.ufrn.nei.almoxarifadoapi.utils.RemoveRolePrefix;
+
+import java.util.List;
 
 public interface UserProjection {
     Long getId();
@@ -12,9 +15,20 @@ public interface UserProjection {
     Boolean getActive();
     @JsonIgnore
     RoleEntity getRole();
+    @JsonIgnore
+    List<RecordEntity> getRecords();
 
     @JsonProperty("role")
     default String removeRolePrefix() {
         return RemoveRolePrefix.getRoleWithoutPrefix(getRole());
+    }
+
+    @JsonProperty("existRecord")
+    default Boolean existRecord() {
+        if (getRecords().isEmpty()) {
+            return Boolean.FALSE;
+        } else {
+            return Boolean.TRUE;
+        }
     }
 }
